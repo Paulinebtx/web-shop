909d7602b53f8e66d80da344a38f161899e7d238
pauline_btx <boiteux.pauline@gmail.com>
2021-08-25 16:38:17 +0200

    e

--- authenticationuser/models.py
+++ authenticationuser/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your models here.
 class Customer(models.Model):
