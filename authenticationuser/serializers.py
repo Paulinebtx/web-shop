ff47d53dd4c4e7a000efaf863a9265f0b4458769
pauline_btx <boiteux.pauline@gmail.com>
2021-08-25 16:46:30 +0200

    e

--- authenticationuser/serializers.py
+++ authenticationuser/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Customer  
-from django.contrib.auth.models import permissions, User
+from django.contrib.auth.models import User
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
